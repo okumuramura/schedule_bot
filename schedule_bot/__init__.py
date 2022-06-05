f86765079cb0d2389b9de994ef4d9387f06ee0f0
okumuramura <bugagaga88@gmail.com>
2022-06-05 18:05:22 +0400

    new manager format

--- schedule_bot/__init__.py
+++ schedule_bot/__init__.py
@@ -1,7 +1,14 @@
 import logging
 import logging.config
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 logging.config.fileConfig(
     './scedule_bot/logger.conf', disable_existing_loggers=False
 )
 logger = logging.getLogger(__name__)
+
+engine = create_engine('sqlite:///lessons.db')
+Base = declarative_base()
+Session = sessionmaker(bind=engine, expire_on_commit=False)
