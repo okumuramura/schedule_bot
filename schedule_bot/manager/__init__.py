f78aa6a2844ce7e0b7a0f54e1d10e8c8299413af
okumuramura <37512567+okumuramura@users.noreply.github.com>
2022-06-05 18:05:22 +0400

    new manager format

---
+++ schedule_bot/manager/__init__.py
@@ -0,0 +1,16 @@
+from typing import Callable, Any
+from functools import wraps
+
+from schedule_bot import Session as SessionCreator
+
+
+def orm_function(func: Callable[..., Any]):  # type: ignore
+    @wraps(func)
+    def wrapper(*args, **kwargs):  # type: ignore
+        if kwargs.get('session') is None:
+            with SessionCreator() as session:
+                return func(*args, session=session, **kwargs)
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
